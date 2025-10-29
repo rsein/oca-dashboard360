a8a7ea24a110df17c67648508245a6b993be3e73
Rsein Estúdio <rodrigo@rseinestudio.com>
2025-10-29 11:01:34 -0300

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,24 @@
+// Senha única para acesso
+const MASTER_PASSWORD = "OCA2025#medsenior";
+
+// Checa se está autenticado
+function isAuthenticated() {
+  return localStorage.getItem("authenticated") === "true";
+}
+
+// Faz login
+function login(password) {
+  if (password === MASTER_PASSWORD) {
+    localStorage.setItem("authenticated", "true");
+    window.location.href = "index.html";
+  } else {
+    alert("Senha incorreta!");
+  }
+}
+
+// Protege as páginas
+function protectPage() {
+  if (!isAuthenticated()) {
+    window.location.href = "login.html";
+  }
+}
